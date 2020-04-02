b0f8dd4eeb3c8149b4a83f5d9714ec61f7e7a0a8
JY-summer <luojinye0@gmail.com>
2020-04-02 23:09:10 +0800

    周四

--- Project1/def.h
+++ Project1/def.h
@@ -3,31 +3,31 @@
 #include <ros/ros.h>
 #include <ros/package.h>
 #include "std_msgs/String.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 using namespace std;
+
+//长度单位m，角度单位rad
 
 struct position {
 	double x, y, z;
-	// 单位与GPS坐标一致。
 };
 
-struct attitude
-{
-	vector <double> att;
-};
 
-struct tri_sp {
-	double x_sp, y_sp, z_sp; //m/s
+struct velocity {
+	double x_sp, y_sp, z_sp; 
 };
 
 struct ang_rate
 {
 	double roll_r, pitch_r, yaw_r;
 };
+
 struct plane {
 	position pos;
-	attitude att;
-	tri_sp t_sp;
+	velocity v;
 	ang_rate ang_r;
-	double air_speed; //单位m/s
-	double speed; //单位m/s
-};
+	double air_speed; //不知道空速发布的话题是什么
+	double v_body;//不知道能不能直接收到机体速度
+	geometry_msgs / Quaternion attitude_qv;  //四元数
+}plane;
--- Project1/subscribe.cpp
+++ Project1/subscribe.cpp
@@ -8,49 +8,62 @@ struct Subscriber
 	ros::Subscriber v_local;
 	ros::Subscriber v_body;
 	ros::Subscriber v_body_cov;
+	ros::Subscriber att;
 }sub;
 
 void posInit(const std_msgs::String::ConstPtr& msg)
 {
 	plane.pos.x = msg->pose.position.x;
-	mocap_ned_pose.position.y = msg->pose.position.y;
-	mocap_ned_msg.pose.position.z = msg->pose.position.z;
-	mocap_ned_msg.pose.orientation.x = msg->pose.orientation.x;
-	mocap_ned_msg.pose.orientation.y = msg->pose.orientation.y;
-	mocap_ned_msg.pose.orientation.z = msg->pose.orientation.z;
-	mocap_ned_msg.pose.orientation.w = msg->pose.orientation.w;
-	 //赋值
+	plane.pos.y = msg->pose.position.y;
+	plane.pos.z = msg->pose.position.z;
 }
 
-void pos_covInit(const std_msgs::String::ConstPtr& msg)
-{
-	//赋值
-}
 
-void odomInit(const std_msgs::String::ConstPtr& msg)
-{
-}
 
 void v_localInit(const std_msgs::String::ConstPtr& msg)
 {
+	plane.v.x_sp = msg->twist.linear.x;
+	plane.v.y_sp = msg->twist.linear.y;
+	plane.v.z_sp = msg->twist.linear.z;
 }
 
 void v_bodyInit(const std_msgs::String::ConstPtr& msg)
 {
+	//因为不知道它发的是什么东西，可能是机体速度，先打出来看看。也不知道它的data是什么东西
+	ROS_INFO("node_b is receiving [%s]", msg->data.c_str());
 }
 
-void v_body_covInit(const std_msgs::String::ConstPtr& msg)
+void att_cb(const sensor_msgs::Imu::ConstPtr& msg)
+{
+	 plane.attitude_qv = Quaterniond(msg->orientation.w, msg->orientation.x, msg->orientation.y, msg->orientation.z);
+	 //保存四元数，未处理,不知道这个赋值函数能不能用
+
+	plane.ang_r.roll_r = msg->angular_velocity.x;
+	plane.ang_r.pitch_r = msg->angular_velocity.x;
+	plane.ang_r.yaw_r = msg->angular_velocity.x;
+}
+
+/*void v_body_covInit(const std_msgs::String::ConstPtr& msg)
 {
 }
+void pos_covInit(const std_msgs::String::ConstPtr& msg)
+{
+	//赋值
+}
+void odomInit(const std_msgs::String::ConstPtr& msg)
+{
+
+}*/
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "subscibe");
 	ros::NodeHandle a;
 	sub.pose = a.subscribe("pose", 10, posInit);
-	sub.pose_cov = a.subscribe("pose_cov", 10, pos_covInit);
+	sub.att = a.subscribe("/mavros/imu/data", 10, att_cb);
 	sub.v_local = a.subscribe("velocity_local", 10, v_localInit);
 	sub.v_body = a.subscribe("velocity_body", 10, v_bodyInit);
+	sub.pose_cov = a.subscribe("pose_cov", 10, pos_covInit);
 	sub.v_body_cov = a.subscribe("velocity_body_cov", 10, v_body_covInit);
 	sub.odom = a.subscribe("odom", 10, odomInit);
 	ros::spin();
