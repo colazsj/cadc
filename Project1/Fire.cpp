f1dc1353ecaa6f0a7de132a1730d828afdb7ea66
JY-summer <luojinye0@gmail.com>
2020-04-24 14:27:50 +0800

    三维速度得到的投弹判断

--- Project1/Fire.cpp
+++ Project1/Fire.cpp
@@ -7,12 +7,14 @@ bool myplane::Drop(float x, float y, float z)
 	float tx = x - pos.x - v.x_sp * delay_t;
 	float ty = y - pos.y - v.y_sp * delay_t;
 	float tz = z - pos.z - v.z_sp * delay_t;//坐标变换，得到靶标相对飞机的位矢在ENU坐标系下。
-
 	pos.z += v.z_sp * delay_t;
-	attitude[2] +=ang_r.pitch_r*delay_t;
-	float h_t = sqrt(2 * pos.z / g);
-	float s_x = v.x_sp * h_t;
-	float s_y = v.y_sp * h_t;
+	attitude[2] +=ang_r.pitch_r*delay_t;//抬头是负，pitch的加速度比较小，如果考虑上误差可能有点大，看试验结果调整
+	attitude[3] += ang_r.yaw_r * delay_t;//正东为0，逆时针为正
+	float t1 = v.z_sp / g;//用飞机的竖直速度分量代水瓶竖直速度，可能有误差，之后再改进,上面是用俯仰角和偏航角的思路
+	float h = pos.z + v.z_sp * v.z_sp / (2 * g);
+	float t_h = sqrt(2 * h / g);
+	float s_x = v.x_sp * (t_h+t1);
+	float s_y = v.y_sp * (t_h+t1);
 
 	float d_val = sqrt((tx - s_x) * (tx - s_x) + (ty - s_y) * (ty - s_y));
 
